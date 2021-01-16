0919aee625e064a5c25b1bc5ff1d1f91301a4f8c
aswanth <aswanthaa@gmail.com>
2021-01-16 23:57:04 +0530

    unable to implement swagger header basic authentication

--- src/main/java/com/javaproject/springboot/SpringbootPostgresqlCurdOperationsApplication.java
+++ src/main/java/com/javaproject/springboot/SpringbootPostgresqlCurdOperationsApplication.java
@@ -1,6 +1,7 @@
 package com.javaproject.springboot;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,9 +12,14 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.BasicAuth;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.Parameter;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -26,19 +32,19 @@ public class SpringbootPostgresqlCurdOperationsApplication {
 	}
 
 	
-//	@Bean
-//	public Docket swaggerConfiguration() {
-//		
-//		return new Docket(DocumentationType.SWAGGER_2)
-//				.enableUrlTemplating(true)
-//				.select()
-//				.paths(PathSelectors.ant("/api/**"))
-//				//.paths(PathSelectors.any())
-//				.apis(RequestHandlerSelectors.basePackage("com.javaproject"))
-//				.build()
-//		//configuring metadata for the apis
-//				.apiInfo(apiDetails());
-//	}
+	@Bean
+	public Docket swaggerConfiguration() {
+		
+		return new Docket(DocumentationType.SWAGGER_2)
+				.enableUrlTemplating(true)
+				.select()
+				.paths(PathSelectors.ant("/api/**"))
+				//.paths(PathSelectors.any())
+				.apis(RequestHandlerSelectors.basePackage("com.javaproject"))
+				.build()
+		//configuring metadata for the apis
+				.apiInfo(apiDetails());
+	}
 	private ApiInfo apiDetails() {
 		return new ApiInfo(
 				"Employee Curd Operation APIs",
@@ -53,24 +59,53 @@ public class SpringbootPostgresqlCurdOperationsApplication {
 				);
 	}
 	
-    @Bean
-    public Docket api() {
-        //Adding Header
-        ParameterBuilder aParameterBuilder = new ParameterBuilder();
-        aParameterBuilder.name("Authorization")                 // name of header
-                         .modelRef(new ModelRef("string"))
-                         .parameterType("header")               // type - header
-                         .defaultValue("Basic dXNlcjpwYXNzd29yZA==")        // based64 of - user:password
-                         .required(true)                // for compulsory
-                         .build();
-        java.util.List<Parameter> aParameters = new ArrayList<>();
-        aParameters.add(aParameterBuilder.build());             // add parameter
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                                                      .apis(RequestHandlerSelectors.basePackage("com.javaproject"))
-                                                      .paths(PathSelectors.ant("/api/**"))
-                                                      .build()
-                                                      .pathMapping("")
-                                                      .globalOperationParameters(aParameters)
-                                                      .apiInfo(apiDetails());
-    }
+	
+//    @Bean
+//    public Docket api() {
+//        //Adding Header
+//        ParameterBuilder aParameterBuilder = new ParameterBuilder();
+//        aParameterBuilder.name("Authorization")                 // name of header
+//                         .modelRef(new ModelRef("string"))
+//                         .parameterType("header")               // type - header
+//                         .defaultValue("Basic dXNlcjpwYXNzd29yZA==")        // based64 of - user:password
+//                         .required(true)                // for compulsory
+//                         .build();
+//        java.util.List<Parameter> aParameters = new ArrayList<>();
+//        aParameters.add(aParameterBuilder.build());             // add parameter
+//        return new Docket(DocumentationType.SWAGGER_2).select()
+//                                                      .apis(RequestHandlerSelectors.basePackage("com.javaproject"))
+//                                                      .paths(PathSelectors.ant("/api/**"))
+//                                                      .build()
+//                                                      .pathMapping("")
+//                                                      .globalOperationParameters(aParameters)
+//                                                      .apiInfo(apiDetails());
+//    }
+	
+//	@Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.any())
+//                .paths(PathSelectors.any())
+//                .build()
+//                .securityContexts(Arrays.asList(securityContext()))
+//                .securitySchemes(Arrays.asList(basicAuthScheme()));
+//   }
+//
+//    private SecurityContext securityContext() {
+//        return SecurityContext.builder()
+//                .securityReferences(Arrays.asList(basicAuthReference()))
+//                .forPaths(PathSelectors.ant("/api/**"))
+//                .build();
+//    }
+//
+//    private SecurityScheme basicAuthScheme() {
+//        return new BasicAuth("basicAuth");
+//    }
+//
+//    private SecurityReference basicAuthReference() {
+//        return new SecurityReference("basicAuth", new AuthorizationScope[0]);
+//    }
+//
+//}
 }
